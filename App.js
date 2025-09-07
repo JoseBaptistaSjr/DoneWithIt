890f3961f42cc12eca0e4829b608b99fb3fc4d42
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-09-06 21:31:37 -0300

    no message

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Image, Button, Alert } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
+       <Button 
+       title="Press me"
+       onPress={() => Alert.alert("My title", "My message", [
+         {text: 'Yes', onPress: () => console.log('Yes Pressed')},
+         {text: 'No', onPress: () => console.log('No Pressed')}
+       ])}
+       />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +22,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
